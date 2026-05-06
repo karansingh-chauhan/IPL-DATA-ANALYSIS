40d1014b1edde70c2a41cfda46e274c8b0273b4f
karan singh <karansingh.chauhann18@gmail.com>
2026-05-06 23:42:11 +0530

    rearange of rq

--- pages/tmkoc.py
+++ pages/tmkoc.py
@@ -1,3 +1,6 @@
 import streamlit as st
 import pandas as pd
 st.image(image="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExajlqenFyamt6cnozYjN2dGwxazU1NXA5M3RheHd5eDA3ejF1d2x0NSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/SinIpWR8Jc6VVIdwNy/giphy.gif")
+import plotly
+
+st.write("Plotly version:", plotly.__version__)
